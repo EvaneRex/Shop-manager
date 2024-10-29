5e81b7b7fd4a5f3719310b958cd7d606f19a0db5
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-10-29 12:01:38 +0100

    localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ function App() {
   //opdatere localstorage når varer/shopItems ændres
   useEffect(() => {
     localStorage.setItem("shopItems", JSON.stringify(shopItems));
-  });
+  }, [shopItems]);
+
+  //opdatere localstorage når butikker/shops ændres
+  useEffect(() => {
+    localStorage.setItem("shops", JSON.stringify(shops));
+  }, [shops]);
 
   return ShopInput();
 }
